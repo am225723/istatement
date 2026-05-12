feb83f464f60f0e694f8a1f9e03437eca558367c
am225723 <puertaale@gmail.com>
2026-05-12 19:35:26 -0400

    Redesign sign-up page with brand frame

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,2 +1,10 @@
 import { SignUp } from '@clerk/nextjs';
-export default function Page(){ return <div className="flex min-h-screen items-center justify-center p-6"><SignUp /></div>; }
+import { AuthFrame } from '@/components/auth-frame';
+
+export default function Page() {
+  return (
+    <AuthFrame mode="sign-up">
+      <SignUp routing="path" path="/sign-up" signInUrl="/sign-in" afterSignUpUrl="/app" />
+    </AuthFrame>
+  );
+}
